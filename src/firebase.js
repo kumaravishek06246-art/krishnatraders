07e14bcd2eb8cbc138609a5d923c10ebffa9ea81
kumaravishek06246-art <kumaravishek06246@gmail.com>
2026-08-03 00:11:55 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDz6iDnpD0c_K-ike1f0t6aS6KezBaYKs",
+  authDomain: "krishna-traders-a8849.firebaseapp.com",
+  projectId: "krishna-traders-a8849",
+  storageBucket: "krishna-traders-a8849.firebasestorage.app",
+  messagingSenderId: "115202514278",
+  appId: "1:115202514278:web:33e39bba7cca132b8e3ca7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
